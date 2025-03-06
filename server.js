109a0443cdd741afd939b326dfa754cf8e03a7c8
Rekhansika <rekhagoluguri02@gmail.com>
2025-03-06 12:01:28 +0530

    code

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ping",(req,res)=>{
+app.get("/ping", (req, res) => {
     try {
-        res.status(200).send({msg:"pong"})
+        res.status(200).send({ msg: "pong" });
     } catch (error) {
-        res.status(500).send({msg:"Error occured",error});
+        res.status(500).send({ msg: "Server error occurred", error: error.message });
     }
-})
+});
 
 
-
-
-app.listen(3000,()=>{
-    try {
-        console.log("Server connected successfully");
-    } catch (error) {
-        console.log("Error",error);
+app.listen(3000, (err) => {
+    if (err) {
+        console.error("Failed to start the server. Error:", err.message);
+    } else {
+        console.log("Server connected successfully on port 3000");
     }
-})
+});
