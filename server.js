f3061d46fba7b1b5c72cb03566ae2e516c2537da
Rekhansika <rekhagoluguri02@gmail.com>
2025-03-06 11:47:27 +0530

    code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/ping",(req,res)=>{
+    try {
+        res.status(200).send({msg:"pong"})
+    } catch (error) {
+        res.status(500).send({msg:"Server occured",error});
+    }
+})
+
+app.listen(3000,()=>{
+    try {
+        console.log("Server connected successfully");
+    } catch (error) {
+        console.log("Error",error);
+    }
+})
