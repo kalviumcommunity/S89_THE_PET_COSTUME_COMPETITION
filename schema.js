e5af97fb846dba6d23a735a2b79cdbf04a4d4dee
Rekhansika <rekhagoluguri02@gmail.com>
2025-03-26 15:20:39 +0530

    schema

---
+++ schema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const user = mongoose.Schema({
+    pet_name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    owner_name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    costume:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    category:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    age:{
+        type:Number,
+        required:true,
+        trim:true
+    },
+    species:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    breed:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    prize:{
+        type:String,
+        required:true,
+        trim:true
+    }
+    
+
+})
+
+const pet = mongoose.model("rekha",user);
+module.exports = pet;
