e6697f357d7a2fb174a69dcc0fad117377b5c4bb
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-01 16:12:27 +0530

    about

---
+++ frontend/src/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1> 🦮🐇 The Pet Costume Competition 🐿️🐈</h1>
+      <p style={{fontSize:"large"}}>💫Welcome to the most fun and creative contest for pet lovers..
+      Upload your pet’s funniest costume and vote for the best ones!💫</p>
+      <button style={{background:"black",color:"white"}}>Join now</button>
+    </div>
+  )
+}
+
+export default About
+
+
