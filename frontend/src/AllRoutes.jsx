3c475f403b935b8305ad802136047dabf626b473
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-15 14:13:08 +0530

    code

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom";
 import About from './About';
+import Costume from './components/costume';
+
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<About/>}/>
+        <Route path='costume' element={<Costume/>}/>
     </Routes>
     
   )
