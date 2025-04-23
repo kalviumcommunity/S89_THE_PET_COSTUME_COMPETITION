5b9ae09caa79b2bc5d170bc6aab101b8bbd380ce
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-23 18:05:05 +0530

    form

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Routes,Route} from "react-router-dom";
+import {Routes,Route, Form} from "react-router-dom";
 import About from './About';
 import Costume from './components/costume';
 import Navbar from './Navbar';
 import EntityList from './components/EntityList';
+import FormPage from './components/FormPage';
 
 
 const AllRoutes = () => {
@@ -12,6 +13,7 @@ const AllRoutes = () => {
         <Route path='/' element={<About/>}/>
         <Route path='/costume' element={<Costume/>}/>
         <Route path='/list' element={<EntityList/>}/>
+        <Route path='/addcostume' element={<FormPage/>}/>
     </Routes>
     
   )
